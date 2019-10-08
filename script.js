6c79dd864ee0985e12730e34e31ffe6c0e0ce442
Paul <pdeggington@gmail.com>
2019-10-07 19:25:56 -0700

    TopNav

--- script.js
+++ script.js
@@ -59,39 +59,15 @@ function onValueClicked(element) {
 
 
 
-
-
-
-
-
-
-//on resize, move search and top nav position according to window width
-var resizing = false;
-window.addEventListener('resize', function(){
-  if(resizing) return;
-  resizing = true;
-  (!window.requestAnimationFrame) ? setTimeout(moveNavigation, 420) : window.requestAnimationFrame(moveNavigation);
-});
-window.dispatchEvent(new Event('resize'));//trigger the moveNavigation function
-
-function moveNavigation(){
-  var mq = checkMQ();
-  if ( mq == 'mobile' && !Util.hasClass(navList.parentNode, 'js-cd-side-nav') ) {
-    detachElements();
-    sidebar.appendChild(navList);
-    sidebar.insertBefore(searchInput, sidebar.firstChild);
-  } else if ( mq == 'desktop' && !Util.hasClass(navList.parentNode, 'js-cd-main-header') ) {
-    detachElements();
-    mainHeader.appendChild(navList);
-    mainHeader.insertBefore(searchInput, mainHeader.firstChild.nextSibling);
+function topNav() {
+  var x = document.getElementById("myTopnav");
+  if (x.className === "topNav") {
+    x.className += " responsive";
+  } else {
+    x.className = "topNav";
   }
-  resizing = false;
-};
+}
 
-function detachElements() {
-  searchInput.parentNode.removeChild(searchInput);
-  navList.parentNode.removeChild(navList);
-};
 
 
 
